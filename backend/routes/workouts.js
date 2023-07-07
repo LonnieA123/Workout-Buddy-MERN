8a6f7b4043bc2ab0f799787e30b4fa64d83ff915
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-07-07 18:01:39 -0500

    "comments on routes file"

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const Workout = require('../models/workoutModel')
 const { postWorkout,
-        getWorkouts,
-        getWorkout,
-        updateWorkout,
-        deleteWorkout} = require('../controllers/workoutController')
+                getWorkouts,
+                getWorkout,
+                updateWorkout,
+                deleteWorkout
+                        } = require('../controllers/workoutController')
 const router = express.Router()
 router.get('/', getWorkouts)
 
+//get specific workout
 router.get('/:id',getWorkout)
 
 //post a workout
 router.post('/', postWorkout)
 
+//delete workout
 router.delete('/:id',deleteWorkout)
 
+//update workout
 router.patch('/:id', updateWorkout)
 
 module.exports = router
