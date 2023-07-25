7ded5ac0eb5d4818cb2e8812bdd66cb4c0354945
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-07-24 20:30:00 -0500

    "added react context and adding delete"

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,26 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
-//how the different workouts will be presented
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
-            <p>Load: <strong>{workout.load}</strong></p>
-            <p>Reps: <strong>{workout.reps}</strong></p>
+            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Number of reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
